199ef0b658c07d667e33fbb04643eda930b83c6f
Gleb Pautov <pautov.split@mail.ru>
2020-10-19 11:36:58 +0500

    done

--- VodoKanalForms/Form1.cs
+++ VodoKanalForms/Form1.cs
@@ -18,24 +18,33 @@ namespace VodoKanalForms
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            //Вывод данных
-            string table1 = "Fucker"; //Имя таблицы
-            string ssql = $"SELECT  * FROM {table1} "; //ЗАпрос 
-
-            string connectionString = @"Data Source=DESKTOP-EE4RPR4\\SQLEXPRESS;Initial Catalog=commentDB;Integrated Security=True"; //prak - имя базы данных(Поменять на свою)
+            string connectionString = @"Data Source=DESKTOP-EE4RPR4\SQLEXPRESS;Initial Catalog=commentDB;Integrated Security=True"; //prak - имя базы данных(Поменять на свою)
             SqlConnection conn = new SqlConnection(connectionString); // Подключение к БД
             conn.Open();// Открытие Соединения
-
-            SqlCommand command = new SqlCommand(ssql, conn);// Объект вывода запросов
-            SqlDataReader reader = command.ExecuteReader(); // Выполнение запроса вывод информации
-
-            while (reader.Read()) //В цикле вывести всю информацию из таблици
+            
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
             {
-                textBox1.Text += reader[0];
-                textBox1.Text += reader[1];
-                textBox1.Text += reader[2];
+                MessageBox.Show("Вы должны заполнить все поля!");
+            } 
+            else
+            {
+                SqlCommand command = new SqlCommand("INSERT INTO [ReviewsData] (Name, Surname, Patronymic, Rating, Review) VALUES(@Name, @Surname, @Patronymic, @Rating, @Review)", conn);// Объект вывода запросов
+                command.Parameters.AddWithValue("Name", textBox2.Text);
+                command.Parameters.AddWithValue("Surname", textBox3.Text);
+                command.Parameters.AddWithValue("Patronymic", textBox4.Text);
+                command.Parameters.AddWithValue("Rating", numericUpDown1.Value);
+                command.Parameters.AddWithValue("Review", textBox1.Text);
+
+                try
+                {
+                    await command.ExecuteNonQueryAsync();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
         }
 
