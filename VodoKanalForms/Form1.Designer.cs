5eae5dd9b7ce95f86c2b4068ac72d63d84ce0922
Gleb Pautov <pautov.split@mail.ru>
2020-10-19 11:40:16 +0500

    done1

--- VodoKanalForms/Form1.Designer.cs
+++ VodoKanalForms/Form1.Designer.cs
@@ -132,7 +132,7 @@
             this.Controls.Add(this.textBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Name = "Form1";
-            this.Text = "Dios Mio";
+            this.Text = "Оценка качества услуг";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
